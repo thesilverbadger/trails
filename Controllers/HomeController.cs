51b5cc6bec3b589aaf981b012756c16b5f6930be
Tom Wire <tom.w.wire@gmail.com>
2016-02-28 20:01:25 +0000

    Deployment sync

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,14 +11,14 @@ namespace trails.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Following along with the Pluralsight ASP.NET Core 1.0 Play by Play course.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "The contact page.";
 
             return View();
         }
